f01a2e9a11001325e6513f94d8baba6ac9e57f64
AmolHarsh <amolharsh123@gmail.com>
2022-01-24 17:10:43 +0530

    Anagram checker specially curated for Abhigyan

--- Message.py
+++ Message.py
@@ -1,8 +1,30 @@
-print ("Hello Abhigyan! from Prayagraj")
+print ("Hello Abhigyan! from Prayagraj.\nWelcome the to the world of Anagram Checker!")
 
-b = input("Enter your salary")
-a = input("Enter your investment")
+def Anagram(first_word, second_word):
+    
+    List1 = []
+    List2 = []
+    
+    #string to list conversion
+    for alpha in first_word:
+        List1.append(alpha)
+    
+    for alpha in second_word:
+        List2.append(alpha)
+        
+    #checking whether the word is an anagram or not.
+    for alpha in List1:
+        if alpha in List2:
+            List2.remove(alpha)
+        else:
+            print("The word: ", second_word," is not an anagram of the word:", first_word)
+            return
+    print ("The word: ", second_word," is an anagram of the word: ", first_word)
 
-c = b/a
 
-print("Your investment vs salary ratio is", c)
+first_word = input("Enter your first word! eg. Aurobindo,Paneer! Beaware the program is case-sensitive.\n")
+second_word = input("Enter your second word\n")
+Anagram(first_word, second_word)
+
+
+        
